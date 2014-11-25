f652578c46e384bfb213546b82558efe50c65283
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2014-11-24 17:21:50 -0700

    debug only localhost only

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-  error_reporting(E_ALL ^ E_NOTICE);
-  ini_set('display_errors',1);
+  if($_SERVER['SERVER_NAME'] == 'localhost'):
+    error_reporting(E_ALL ^ E_NOTICE);
+    ini_set('display_errors',1);
+  endif;
 
   require_once(dirname(__FILE__).'/app/index.php');
